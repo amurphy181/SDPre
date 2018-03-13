d11668213dfca1bd6c3012cf14a52e81261a3cba
Alex Murphy <alexander.f.murphy@gmail.com>
2018-03-13 16:28:31 -0400

    New exercise 2 file.

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,20 @@
+public class GasolineReceipt {
+  public static void main(String[] args){
+
+    // modified the code to have an extra space between the (") and (+) or (|)
+    // basically just to make everything prettier once executed
+    System.out.println(" +------------------------+");
+    System.out.println(" |                        |");
+    System.out.println(" |      CORNER STORE      |");
+    System.out.println(" |                        |");
+    System.out.println(" |   2018-03-13   16:15   |");
+    System.out.println(" |                        |");
+    System.out.println(" |  Gallons:      12.067  |");
+    System.out.println(" |  Price/gallon: $2.899  |");
+    System.out.println(" |                        |");
+    System.out.println(" |  Fuel total:   $34.98  |");
+    System.out.println(" |                        |");
+    System.out.println(" +------------------------+");
+
+  }
+}

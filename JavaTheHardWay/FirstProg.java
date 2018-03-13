237a68aa966e4a112ad8f287d6ccb60922ed45b5
Alex Murphy <alexander.f.murphy@gmail.com>
2018-03-13 16:08:37 -0400

    Modified to include my name, date, per exercise instructions.

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
   public static void main(String [] args){
-    System.out.println("I am determined to learn how to code!");
-    System.out.println("I gotta say, today was a good day.");
+    System.out.println("I, Alex Murphy, not of RoboCop fame, am determined to learn how to code!");
+    System.out.println("Today's date is March 13, 2018.");
   }
 }

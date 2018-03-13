362c49ef40caf35f4acdca84a4310ed15c4c7431
Alex Murphy <alexander.f.murphy@gmail.com>
2018-03-13 15:47:29 -0400

    First exercise of JtHW.

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+  public static void main(String [] args){
+    System.out.println("Determined to write some code!");
+    System.out.println("I gotta say, today was a good day.");
+  }
+}

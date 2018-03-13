618160a845850d7c0af28941c1d6b09f7f2d9d19
Alex Murphy <alexander.f.murphy@gmail.com>
2018-03-13 16:02:10 -0400

    Modified the first sentence.

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
   public static void main(String [] args){
-    System.out.println("Determined to write some code!");
+    System.out.println("I am determined to learn how to code!");
     System.out.println("I gotta say, today was a good day.");
   }
 }

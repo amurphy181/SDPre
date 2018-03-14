797000a80e958d66fe1a72d42d9f8e7d4ee54d10
Alex Murphy <alexander.f.murphy@gmail.com>
2018-03-14 12:19:52 -0400

    Escape sequences and their raw power

---
+++ JavaTheHardWay/EscapeSequencesComments.java
@@ -0,0 +1,24 @@
+public class EscapeSequencesComments{
+  public static void main(String[] args){
+    /* escape sequences! Or, how to put a quotation mark inside a quote.
+    Or, how to make tabs and new lines inside a System.out.print. All sorts of "ors." */
+
+    System.out.print("Learn\tJava\n\tthe\nHard\tWay\n\n");
+    System.out.print("\tLearn Java the \"Hard\" Way!\n");
+    // System.out.frimp("Learn Java the Hard Way");
+
+    System.out.print("Hello\n"); // Print me a "hello"
+    System.out.print("Jello\by\n"); // This line prints "Jelly," implying a removal of "o"
+
+    /* The quick brown fox jumped over a lazy dog. << All letters of the alphabet.
+       Quick wafting zephyrs vex bold Jim. */
+
+    System.out /* testing */ .println("Hard to believe, eh?");
+    System.out.println("Surprised? /* abcde */ Or what did you expect?");
+    System.out.println("\\ // -=- \\ //");
+    System.out.println("\\\\ \\\\\\ \\\\\\\\"); // it takes 2 to make 1, eh?
+    System.out.print("I hope you understand \"escape sequences\" now.\n");
+
+    // and maybe even comments!
+  }
+}

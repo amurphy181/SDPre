721a7310c4a931ea2c3fde6db204be8a80229d44
Alex Murphy <alexander.f.murphy@gmail.com>
2018-03-14 17:06:24 -0400

    First go at O2L2, pre-formatting.

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2{
+  public static void main(String[] args){
+
+    String name = "Alex Murphy";
+    int age = 28;
+    String state = "Connecticut";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
+  }
+}

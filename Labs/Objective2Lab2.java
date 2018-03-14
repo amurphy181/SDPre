a1258e38bbe77e89bc2d970d6b059215b5f7a16d
Alex Murphy <alexander.f.murphy@gmail.com>
2018-03-14 17:09:18 -0400

    O2L2 with some new formatting.

--- Labs/Objective2Lab2.java
+++ Labs/Objective2Lab2.java
@@ -6,6 +6,9 @@ public class Objective2Lab2{
     String state = "Connecticut";
 
     System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+    System.out.println("\nOr:\n");
+    System.out.println("Name:\t" + name + "\nAge:\t" + age + "\nState:\t" + state);
+    System.out.println();
 
   }
 }

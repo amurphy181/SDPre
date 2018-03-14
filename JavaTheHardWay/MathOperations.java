0858c950eca029c61016e1e018810ddb0559b0e4
Alex Murphy <alexander.f.murphy@gmail.com>
2018-03-14 16:53:46 -0400

    Study drills added in addition to a bit of dolling up.

--- JavaTheHardWay/MathOperations.java
+++ JavaTheHardWay/MathOperations.java
@@ -2,36 +2,40 @@ public class MathOperations{
   public static void main(String[] args){
     // An exercise to run mathematical operations.
 
-    int a, b, c, d, e, f, g;
-    double x, y, z;
+    int a, b, c, d, e, f, g, h;
+    double w, x, y, z;
     String one, two, both;
 
     a = 10;
     b = 27;
-    System.out.println("a is " + a + ", b is " + b);
+    System.out.println("\na is " + a + ", b is " + b);
 
     c = a + b;
-    System.out.println("a+b is " + c);
-    d = a- b;
-    System.out.println("a-b is " + d);
+    System.out.println("a + b is " + c);
+    d = a - b;
+    System.out.println("a - b is " + d);
     e = a + b * 3;
-    System.out.println("a+b * 3 is " + e);
+    System.out.println("a + b * 3 is " + e);
     f = b / 2;
-    System.out.println("b/2 is " + f);
+    System.out.println("b / 2 is " + f);
     g = b % 10;
-    System.out.println("b%10 is " + g);
+    System.out.println("b % 10 is " + g);
+    h = (b * 6) - f;
+    System.out.println("b * 6 - f is " + h);
 
+    w = 22d / 7d; // added the "d" because it was treating the two as integers
+    System.out.println("\n22 / 7 is " + w);
     x = 1.1;
-    System.out.println("\nx is " + x);
+    System.out.println("x is " + x);
     y = x * x;
-    System.out.println("x*x is " + y);
+    System.out.println("x * x is " + y);
     z = b / 2;
-    System.out.println("b/2 is " + z);
+    System.out.println("b / 2 is " + z);
     System.out.println();
 
     one = "dog";
     two = "house";
     both = one + two;
-    System.out.println(both);
+    System.out.println(both + "\n");
   }
 }

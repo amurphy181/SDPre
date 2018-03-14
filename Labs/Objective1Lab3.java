b0dd186b8f6fccb3d18184af8535ed77c13ca45d
Alex Murphy <alexander.f.murphy@gmail.com>
2018-03-14 12:50:47 -0400

    Name, age, and date printout.

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,13 @@
+public class Objective1Lab3{
+  public static void main(String[] args){
+
+    // Looking for name, age, and state of birth with labels
+
+    System.out.println("Name:\tAlex Murphy");
+    System.out.println("Age:\t28");
+    System.out.println("State:\tConnecticut");
+
+    // tab escape sequence added for organization and visual satisfaction.
+
+  }
+}

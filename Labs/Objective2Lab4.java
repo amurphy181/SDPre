634daa3890a3eefdc4025b6cdc032401736dc335
Alex Murphy <alexander.f.murphy@gmail.com>
2018-03-15 11:48:29 -0400

    O2L4 results, averages for doubles

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,17 @@
+public class Objective2Lab4{
+  public static void main(String[] args){
+
+    double num1, num2, num3, average;
+
+    // Using three arbitrary numbers for the variables listed
+    num1 = 7;
+    num2 = 17;
+    num3 = 29;
+    average = (num1 + num2 + num3)/3;
+
+    System.out.println("\nThis output would result from numbers " +
+                        num1 + ", " + num2 + ", " + num3);
+    System.out.println("\n  " + average + "\n");
+
+  }
+}

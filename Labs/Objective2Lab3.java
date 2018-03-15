2c4ac7443a34db69301b828df1d2692dff571ffd
Alex Murphy <alexander.f.murphy@gmail.com>
2018-03-15 11:38:29 -0400

    O2L3 with a bit of formatting

--- Labs/Objective2Lab3.java
+++ Labs/Objective2Lab3.java
@@ -7,8 +7,9 @@ public class Objective2Lab3{
     food2 = "Cheeseburgers";
     food3 = "Salad with poached eggs";
 
-    System.out.println("My Favorite Foods Are:");
-    System.out.println("1: " + food1 + "\n2: " + food2 + "\n3: " + food3);
-    
+    System.out.println("\nMy Favorite Foods Are:\n");
+    System.out.println(" 1: " + food1 + "\n 2: " + food2 + "\n 3: " + food3);
+    System.out.println();
+
   }
 }

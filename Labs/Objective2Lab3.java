5c03c30f5fc360e0fb0a8b15018bc3d3840d964e
Alex Murphy <alexander.f.murphy@gmail.com>
2018-03-15 11:35:35 -0400

    First run through O2L3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,14 @@
+public class Objective2Lab3{
+  public static void main(String[] args){
+
+    String food1, food2, food3;
+
+    food1 = "Stuffed salmon";
+    food2 = "Cheeseburgers";
+    food3 = "Salad with poached eggs";
+
+    System.out.println("My Favorite Foods Are:");
+    System.out.println("1: " + food1 + "\n2: " + food2 + "\n3: " + food3);
+    
+  }
+}

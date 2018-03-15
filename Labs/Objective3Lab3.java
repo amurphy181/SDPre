ffeb2edf6952faa2f8292799d7f7825c3770f11f
Alex Murphy <alexander.f.murphy@gmail.com>
2018-03-15 16:00:42 -0400

    Altered this slightly to allow for a more accurate birth year.

--- Labs/Objective3Lab3.java
+++ Labs/Objective3Lab3.java
@@ -4,6 +4,8 @@ public class Objective3Lab3{
   public static void main(String[] args){
 
     int birthYear, age, currentYear;
+    String birthday;
+
     Scanner input = new Scanner(System.in);
 
     birthYear = 0;
@@ -12,11 +14,24 @@ public class Objective3Lab3{
     System.out.println("How old are you?");
     age = input.nextInt();
 
-    birthYear = currentYear - age;
-
-    System.out.println("You were born in " + birthYear);
+    System.out.println("Have you had a birthday this year?");
+    birthday = input.next();
 
     input.close();
+
+    /* Tried this next bit originally with "if (birthday == "yes")" etc., but found that
+       it did not work! Went on stack overflow where someone had helpfully cleared up the way
+       to test for string matches. */
+       
+    if (birthday.equals("Yes") || birthday.equals("yes") || birthday.equals("Y") || birthday.equals("y")){
+      birthYear = currentYear - age;
+      System.out.println("You were born in " + birthYear);
+    } else if (birthday.equals("No") || birthday.equals("no") || birthday.equals("N") || birthday.equals("n")){
+      birthYear = currentYear - age - 1;
+      System.out.println("You were born in " + birthYear);
+    } else {
+      System.out.println("So you don't have birthdays?");
+    }
 
   }
 }

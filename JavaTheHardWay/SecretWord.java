2ecd34814265989fe0a9a38984973e5d265aac06
Alex Murphy <alexander.f.murphy@gmail.com>
2018-03-16 15:49:16 -0400

    JtHW Exercise 18 with Study Drill additions

--- JavaTheHardWay/SecretWord.java
+++ JavaTheHardWay/SecretWord.java
@@ -4,12 +4,12 @@ public class SecretWord{
   public static void main(String[] args){
 
     Scanner input = new Scanner(System.in);
-    String secret = "please", guess;
+    String secret = "please", secret2 = "barnyard", guess;
 
     System.out.print("What's the secret word? ");
     guess = input.next();
 
-    if (guess.equals(secret)){
+    if (guess.equals(secret) || guess.equals(secret2)){
       System.out.println("That's correct!");
     } else {
       System.out.println("No, the secret word isn't \"" + guess + "\".");

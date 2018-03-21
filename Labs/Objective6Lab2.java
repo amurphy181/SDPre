d85c3a73c59291531a0dec731848b797d4efa9d9
Alex Murphy <alexander.f.murphy@gmail.com>
2018-03-21 13:20:43 -0400

    O6L2 completed exercise

---
+++ Labs/Objective6Lab2.java
@@ -0,0 +1,22 @@
+public class Objective6Lab2{
+  public static void main(String[] args){
+
+    int counter = 0;
+
+    // counter below counts 0 - 20, inclusive of both
+    while(counter <= 20){
+      System.out.println(counter);
+      counter++;
+    }
+
+    // counter below counts 1 - 20
+
+    /*
+    counter = 1;
+    while(counter <= 20){
+      System.out.println(counter);
+      counter++;
+    } */
+
+  }
+}

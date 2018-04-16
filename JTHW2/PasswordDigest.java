fa7a256af8586777d83cb05b364538d2a0af4b6f
Alex Murphy <alexander.f.murphy@gmail.com>
2018-04-16 16:07:40 -0600

    the next stage

---
+++ JTHW2/PasswordDigest.java
@@ -0,0 +1,21 @@
+import java.security.MessageDigest;
+import java.util.Scanner;
+import javax.xml.bind.DatatypeConverter;
+
+public class PasswordDigest{
+  public static void main(String[] args) throws Exception{
+  Scanner input = new Scanner(System.in);
+  String pw, hash;
+
+  MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+  System.out.println("Password: ");
+  pw = input.nextLine();
+
+  digest.update(pw.getBytes("UTF-8"));
+  hash = DatatypeConverter.printHexBinary(digest.digest());
+
+  System.out.println(hash);
+
+ }
+}
